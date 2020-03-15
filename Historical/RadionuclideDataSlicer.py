abd1a99ad86f588f0df0c095e72cd6b4cded6329
Peter Madsen <peter.madsen784@myci.csuci.edu>
2020-03-14 20:29:03 -0700

    Created "Historical" folder and moved old files there.

---
+++ Historical/RadionuclideDataSlicer.py
@@ -0,0 +1,36 @@
+# Dicing up the SpectraMatrix data into its respective boxes
+# This code is really cool but also very dangerous to run again
+# so make sure file1, file2, etc. remain commented out.
+
+import pandas as pd
+
+box1 = open("../data files/Box1.csv", "w")  # This will be 0, 4, 8, 12... 1760
+box2 = open("../data files/Box2.csv", "w")  # 1, 5, 9, 13... 1761
+box3 = open("../data files/Box3.csv", "w")  # 2, 6, 10, 14... 1762
+box4 = open("../data files/Box4.csv", "w")  # 3, 7, 11, 15... 1763
+
+SpectraMatrix = pd.read_csv("../data files/SpectraMatrix.csv", header=None, index_col=None)
+
+# file1 = open("data files/Box1.csv", mode="w", newline="")
+# file2 = open("data files/Box2.csv", mode="w", newline="")
+# file3 = open("data files/Box3.csv", mode="w", newline="")
+# file4 = open("data files/Box4.csv", mode="w", newline="")
+
+for j in range(200):
+    row = j
+    for i in range(441):
+        column = i * 4
+        print(f"{SpectraMatrix.iloc[row, (column+3)]} is row {row}, column {column+3}")
+        file1.write(f"{SpectraMatrix.iloc[row, column]},")
+        file2.write(f"{SpectraMatrix.iloc[row, (column+1)]},")
+        file3.write(f"{SpectraMatrix.iloc[row, (column+2)]},")
+        file4.write(f"{SpectraMatrix.iloc[row, (column+3)]},")
+    file1.write("\n")
+    file2.write("\n")
+    file3.write("\n")
+    file4.write("\n")
+
+file1.close()
+file2.close()
+file3.close()
+file4.close()
