aa2519c6de29d0d9b93646c2b7a2bb8cab6c1a1d
Peter Madsen <peter.madsen784@myci.csuci.edu>
2020-03-20 17:33:14 -0700

    Began CPT stuff

--- Scratchpad.py
+++ Scratchpad.py
@@ -1,14 +1,30 @@
-# This was a quick script to create Box 5, which is made of
-# Box2 + Box3 + Box4 - 2*Box1, after unnormalizing (denormalizing? Strangifying?)
+# TensorFlow can use CUDA 10.1 to accelerate tensor stuff using
+# the GPU. Currently it lags a bit when the code runs because
+# the library is searching for CUDA stuff and can't find it,
+# so switches to CPU instead. If we start doing crazy stuff,
+# it might be interesting to employ the GPU to do calculations
+# really really fast.
 
 import pandas as pd
 import ThingsWeDoALot as th
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import tensorflow as tf
 
-Box1withoutXAxis = pd.read_csv('data files/Box1.csv', header=None, index_col=None)
-print(Box1withoutXAxis)
-Box1 = th.attachXAxis(dataframe=Box1withoutXAxis)
-print(Box1)
-th.linePlotTheThing(dataframetoplot=Box1, columntoplot=221, title='Spectrum at sensor 220', islogscale=True)
+SpectraMatrix = th.makeDataFrameWithTheXAxis(dataframefilepath='data files/SpectraMatrix.csv')
+
+thetensor = tf.constant([
+    [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]],
+    [[10, 11, 12],
+     [13, 14, 15],
+     [16, 17, 18]],
+    [[19, 20, 21],
+     [22, 23, 24],
+     [25, 26, 27]]])
+
+tf.print(thetensor)
+tf.print(thetensor.shape)
+
